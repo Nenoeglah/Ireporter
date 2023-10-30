906680b7075eba6a8c3866fc9f222c95b7adc3ef
Nenoeglah <eglah.chepngeno@student.moringaschool.com>
2023-10-30 22:22:28 +0300

    working on cloudinary

---
+++ server/utils.py
@@ -0,0 +1,10 @@
+# utils.py
+
+import cloudinary
+
+# Configure Cloudinary with your credentials
+cloudinary.config(
+    cloud_name="your_cloud_name",
+    api_key="your_api_key",
+    api_secret="your_api_secret"
+)
