fadf4e568e10271d52481dba7b20f65d6c4f0b23
Nenoeglah <eglah.chepngeno@student.moringaschool.com>
2023-11-01 17:00:47 +0300

    added some components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,123 @@
+// 🎙
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  const HandleLogout = () => {
+    fetch("/logout", {
+      method: "DELETE"
+    })
+    .then((r) => { 
+      if (r.ok) {
+        setUser(null);
+      } else {
+        r.json().then("There was an error in logging you out")
+      }
+    })
+  }
+
+  return (
+    <nav className="fixed-top navbar navbar-expand-sm  navbar-expand-lg navbar-dark bg-light static-top" >
+      <div className="container-fluid">
+      <Link to={"/"} onClick={() => {window.location.href = "#home"}} className="navbar-brand" style={{ fontWeight: "bold",color: "#fa7670",padding:"2px" }} >
+      iREPO{/* <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/facebook/327/eyes_1f440.png" alt="..." height="36"/>*/}RTER
+      </Link>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarSupportedContent"
+        > 
+          <ul className="navbar-nav">
+          {user ? (  
+            <>
+          <li className="nav-item">
+              <Link
+                className="nav-link d-flex justify-content-center"
+                to="/profile"
+                //to signup
+                style={{ color: "#fa7670" }}
+              >
+                {user.name}'s Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+            <Link
+              className="nav-link d-flex justify-content-center"
+              to="/user-landing"
+              //to signup
+              style={{ color: "#fa7670" }}
+            >
+             Raise an issue
+            </Link>
+          </li>
+          <li className="nav-item">
+          <Link
+            className="nav-link d-flex justify-content-center"
+            to="/"
+            onClick={HandleLogout}
+            //to signup
+            style={{ color: "#fa7670" }}
+          >
+            Logout
+          </Link>
+        </li>
+        </>
+            ) : (
+            <>
+            {/* <li className="nav-item">
+              <Link
+                className="nav-link d-flex justify-content-center"
+                to="/"
+                style={{ color: "black" }}
+
+              >
+                About Us
+              </Link>
+            </li> */}
+               <li className="nav-item">
+              <Link
+                className="nav-link d-flex justify-content-center"
+                onClick={() => {window.location.href = "#how-it-works"}}
+                style={{ color: "#fa7670" }}
+              >
+                How it works
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link d-flex justify-content-center"
+                onClick={() => {window.location.href = "#recent-reports"}}
+                style={{ color: "#fa7670" }}
+              >
+                Recent Reports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link d-flex justify-content-center"
+                to="/get-started"
+                //to signup
+                style={{ color: "#fa7670" }}
+              >
+                Get started
+              </Link>
+            </li>
+          </>
+          )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/User.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { IoPerson } from "react-icons/io5";
+import { BsPhoneVibrate } from "react-icons/bs";
+import Error from "../styles/Error";
+
+function User({ user, onDeleteUser }) {
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [password, setPassword] = useState("password");
+  const [isAdminButton, setIsAdminButton] = useState(false);
+  const navigate = useNavigate();
+
+  function handleUserDelete() {
+    setIsLoading(true);
+    fetch(`/users/${user.id}`, { method: "DELETE" })
+      .then((res) => {
+        res.json();
+        setIsLoading(false);
+      })
+      .then(() => onDeleteUser(user))
+      .catch((err) => console.error(err));
+  }
+
+  function handleAdminState() {
+    fetch(`/users/${user.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        is_admin: isAdmin,
+        password,
+      }),
+    }).then((r) => {
+      setIsAdmin((isAdmin) => !isAdmin);
+      setPassword(password);
+      if (r.ok) {
+        navigate("/users");
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+    setIsAdminButton((isAdminButton) => !isAdminButton);
+  }
+
+  return (
+    <Card key={user.id}>
+      <UserName>
+        <IoPerson style={{ fontSize: "60px" }} /> {user.name}
+      </UserName>
+      <Email>
+        <MdEmail /> : {user.email}
+      </Email>
+      <PhoneNumber>
+        <BsPhoneVibrate /> : {user.phone_number}
+      </PhoneNumber>
+      <ActionButtonMakeAdmin onClick={handleAdminState}>
+        {user.is_admin == isAdminButton ? "Make Admin" : "Is Admin"}
+      </ActionButtonMakeAdmin>
+      <ActionButtonDelete onClick={handleUserDelete}>
+        {isLoading ? "Deleting..." : "Delete User"}
+      </ActionButtonDelete>
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </Card>
+  );
+}
+
+const Card = styled.div`
+  border-radius: 12px;
+  background-color: papayawhip;
+  box-shadow: 0 0.5em 1em -0.125em rgb(10 10 10 / 10%),
+    0 0 0 1px rgb(10 10 10 / 2%);
+  padding: 16px;
+  margin-bottom: 16px;
+`;
+
+const UserName = styled.h2`
+  font-size: 36px;
+  font-weight: 200;
+  color: teal;
+`;
+
+const Email = styled.h4`
+  font-size: 24px;
+  font-weight: 200;
+  color: darkblue;
+  padding-left: 20px;
+  padding-top: 20px;
+`;
+
+const PhoneNumber = styled.h4`
+  font-size: 24px;
+  font-weight: 200;
+  color: black;
+  padding-left: 20px;
+`;
+
+const ActionButtonMakeAdmin = styled.button`
+  margin: 0 5px;
+  padding: 8px 14px;
+  background: #1577c0;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #fff;
+  outline: 0;
+  font-weight: 300;
+  border-radius: 6px;
+
+  &:hover {
+    opacity: 0.8;
+    transform: scale(1.2);
+  }
+`;
+
+const ActionButtonDelete = styled.button`
+  margin: 0 5px;
+  padding: 8px 14px;
+  background: firebrick;
+  color: #fff;
+  cursor: pointer;
+  border: 1px solid #fff;
+  outline: 0;
+  font-weight: 300;
+  i &:hover {
+    opacity: 0.8;
+    transform: scale(1.2);
+  }
+`;
+
+const FormField = styled.div`
+  &:not(:last-child) {
+    margin-bottom: 12px;
+  }
+`;
+
+export default User;
